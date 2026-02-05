8dbcd60ef96fd27807b0b5fbc8e87a2c99ec0e43
Justin Gardner <justin@rhynorater.com>
2026-02-05 06:04:18 -0500

    1.34.2.0

--- vk-block-patterns/readme.txt
+++ vk-block-patterns/readme.txt
@@ -4,7 +4,7 @@ Donate link:
 Tags: Guternberg, Block Pattern
 Requires at least: 6.5
 Tested up to: 6.9
-Stable tag: 1.34.0.1
+Stable tag: 1.34.2.0
 Requires PHP: 7.4
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
@@ -15,6 +15,12 @@ You can make and register your original custom block patterns.
 When you activate this plugin that create new custom post type for custom block patterns. If you register custom patterns that you can select registered block patterns on edit screen.
 
 == Changelog ==
+
+= 1.34.2 =
+[ Bug fix ] Fix an issue where X-T9 theme patterns were not added to Favorites.
+
+= 1.34.1 =
+[ Other ] Add a version to the information sent to the VK Patterns API
 
 = 1.34.0 =
 [ Other ] It is now possible to obtain X-T9 patterns from the pattern library.
--- vk-block-patterns/vendor/composer/installed.php
+++ vk-block-patterns/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'vektor/vk-block-patterns',
-        'pretty_version' => '1.34.0.1',
-        'version' => '1.34.0.1',
-        'reference' => '621eafae0057f85497e2e8e725fdc1120bfe720e',
+        'pretty_version' => '1.34.2.0',
+        'version' => '1.34.2.0',
+        'reference' => '8a1ab4ef5ee06fba3c8189475b82769878bbf27f',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -20,9 +20,9 @@
             'dev_requirement' => false,
         ),
         'vektor/vk-block-patterns' => array(
-            'pretty_version' => '1.34.0.1',
-            'version' => '1.34.0.1',
-            'reference' => '621eafae0057f85497e2e8e725fdc1120bfe720e',
+            'pretty_version' => '1.34.2.0',
+            'version' => '1.34.2.0',
+            'reference' => '8a1ab4ef5ee06fba3c8189475b82769878bbf27f',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
